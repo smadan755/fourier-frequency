8c60585c36004ded1d4d5b5df8767e64c56f34f6
smadan755 <shivansh.madan@utexas.edu>
2025-03-08 11:49:24 -0600

    dsp library

--- dsp/dft_mat.py
+++ dsp/dft_mat.py
@@ -14,5 +14,26 @@ DFT_mat = np.array([
     [1, w**3, w**6, w**9]
 ])
 
+print(f"This is the input sequence: {x}")
 
-print(clean_complex(np.dot(DFT_mat,x)))
+
+dft_x = clean_complex(np.dot(DFT_mat,x))
+
+
+print(f"This is the DFT of x: {dft_x}")
+
+
+print("This is cool, but lets go back")
+
+
+w = np.exp(1j * (2*np.pi)/len(x))
+
+iDFT_mat = np.array([
+    [1, 1, 1, 1],
+    [1, w, w**2, w**3],
+    [1, w**2, w**4, w**6],
+    [1, w**3, w**6, w**9]
+])
+
+
+print(f"This is the iDFT of x: {clean_complex(np.dot(iDFT_mat,dft_x))/4}")
